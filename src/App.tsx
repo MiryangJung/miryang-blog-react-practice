be36788e0d758982f0d7213bd340fdaea7a63344
MiryangJung <miryang.dev@gmail.com>
2021-08-25 04:43:07 +0900

    :wrench: add craco config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
 function App() {
   return (
     <div className="App">
-
+        <div css={stylea}>
+            Some hotpink text.
+        </div>
     </div>
   );
 }
+
+const stylea = css`
+  color:red;
+`;
 
 export default App;
